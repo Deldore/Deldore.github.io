533c9ebee17285e7aa9d496ba970b1ef9116b1bd
Deldore <103668969+Deldore@users.noreply.github.com>
2023-10-13 19:45:00 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -88,3 +88,11 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
     tg.sendData(product);
     //при клике на основную кнопку отправляем данные в строковом виде
 });
+
+
+let usercard = document.getElementById('usercard');
+
+let p = document.createElement('p');
+p.innerText = `${tg.initDataUnsafe.user.first_name}` +
+    `${tg.initDataUnsafe.user.last_name}`
+usercard.appendChild(p);
