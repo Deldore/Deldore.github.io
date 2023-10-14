6f5b047468e17267a4e39a29fa75c2e8929cc0ba
Deldore <103668969+Deldore@users.noreply.github.com>
2023-10-14 16:40:16 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,49 +1,76 @@
-var tg = window.Telegram.WebApp;
+(async () => {
+    var tg = window.Telegram.WebApp;
 
-tg.expand();
-tg.MainButton.textColor = "#FFF";
-tg.MainButton.show();
-tg.MainButton.setText('Добавить отзыв')
+    tg.expand();
+    tg.MainButton.textColor = "#FFF";
+    tg.MainButton.show();
+    tg.MainButton.setText('Добавить отзыв')
 
-let str = "";
+    let str = "";
+    let response = await fetch('https://deldore.github.io/feedback.json');
 
-let feedbacks = JSON.parse(localStorage.getItem('feedback'));
+    // let feedbacks = await response.json();
+    // feedbacks = JSON.parse(feedbacks);
+    let feedbacks = [
+        ['Emil', 'text'],
+    ];
+    response = await fetch('', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(feedbacks)
+    });
 
-for(let i = 0; i < feedbacks.length; i++)
-{
-    str += "<div class=\"item\">\n" +
-        "                <span class='name'>"+feedbacks[i][0]+"</span>\n" +
-        "                <br />\n" +
-        "                <div style=\"\" class=\"value\">"+feedbacks[i][1]+"</div>\n" +
-        "            </div>"
-}
-document.getElementById('inner').innerHTML = str;
-console.log(str);
-let data = JSON.stringify(feedbacks);
-console.log(data);
-
-Telegram.WebApp.onEvent('mainButtonClicked', function(){
-    tg.MainButton.hide();
-    document.getElementById('inner').style.display = 'none';
-    document.getElementById('area').style.display = '';
-    document.getElementById('send').onclick = function () {
-        feedbacks[feedbacks.length] = [
-            `${tg.initDataUnsafe.user.first_name}`+ " " +`${tg.initDataUnsafe.user.last_name}`,
-            document.getElementById('text-area').value,
-        ];
-        str = "";
-        for(let i = 0; i < feedbacks.length; i++)
-        {
-            str += "<div class=\"item\">\n" +
-                "                <span class='name'>"+feedbacks[i][0]+"</span>\n" +
-                "                <br />\n" +
-                "                <div style=\"\" class=\"value\">"+feedbacks[i][1]+"</div>\n" +
-                "            </div>"
-        }
-        document.getElementById('inner').innerHTML = str;
-        document.getElementById('inner').style.display = '';
-        document.getElementById('area').style.display = 'none';
-        tg.MainButton.show();
-        localStorage.setItem('feedback', JSON.stringify(feedbacks));
+    let result = await response.json(); 
+    alert(result.message);
+    for(let i = 0; i < feedbacks.length; i++)
+    {
+        str += "<div class=\"item\">\n" +
+            "                <span class='name'>"+feedbacks[i][0]+"</span>\n" +
+            "                <br />\n" +
+            "                <div style=\"\" class=\"value\">"+feedbacks[i][1]+"</div>\n" +
+            "            </div>"
     }
-});
+    document.getElementById('inner').innerHTML = str;
+    console.log(str);
+    let data = JSON.stringify(feedbacks);
+    console.log(data);
+
+    Telegram.WebApp.onEvent('mainButtonClicked', function(){
+        tg.MainButton.hide();
+        document.getElementById('inner').style.display = 'none';
+        document.getElementById('area').style.display = '';
+        document.getElementById('send').onclick = function () {
+            feedbacks[feedbacks.length] = [
+                `${tg.initDataUnsafe.user.first_name}`+ " " +`${tg.initDataUnsafe.user.last_name}`,
+                document.getElementById('text-area').value,
+            ];
+            str = "";
+            for(let i = 0; i < feedbacks.length; i++)
+            {
+                str += "<div class=\"item\">\n" +
+                    "                <span class='name'>"+feedbacks[i][0]+"</span>\n" +
+                    "                <br />\n" +
+                    "                <div style=\"\" class=\"value\">"+feedbacks[i][1]+"</div>\n" +
+                    "            </div>"
+            }
+            document.getElementById('inner').innerHTML = str;
+            document.getElementById('inner').style.display = '';
+            document.getElementById('area').style.display = 'none';
+            tg.MainButton.show();
+            (async () => {
+            let response = await fetch('', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(user)
+            });
+
+            let result = await response.json();
+            alert(result.message);
+            })()
+        }
+    });
+})()
